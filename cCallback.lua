33e708ea0dba84f13cc1d5000407d1dacade6aa5
OMikkel <57924435+OMikkel@users.noreply.github.com>
2021-04-29 22:13:45 +0200

    Update cCallback.lua

--- cCallback.lua
+++ cCallback.lua
@@ -1,13 +1,27 @@
+-- LAVET I SAMARBEJDE MED MOHR
+-- sCallback.lua og cCallback.lua kan bruges som man vil, dog værdsættes en smule credit, men vigtigst af alt, SÅ SKAL MAN IKKE SÆLGE DET
+-- Link: https://github.com/OMikkel/omik_callbacks
+
 cCallback = {
     TriggerServerCallback = function(self, name, args, cb)
-        TriggerServerEvent("mo-callback:TriggerServerCallback", name, args)
-        while self[name] == nil do
+        TriggerServerEvent("omik-callback:TriggerServerCallback", name, args)
+        while self.server[name] == nil do
             Wait(1)
         end
-        cb(self[name])
-    end
+        cb(self.server[name])
+    end,
+    RegisterClientCallback = function(self, name, f)
+        self.client[name] = f
+    end,
+    server = {},
+    client = {}
 }
-RegisterNetEvent("mo-callback:RecieveServerCallback")
-AddEventHandler("mo-callback:RecieveServerCallback", function(name, data)
-    cCallback[name] = data
+RegisterNetEvent("omik-callback:RecieveServerCallback")
+AddEventHandler("omik-callback:RecieveServerCallback", function(name, data)
+    cCallback.server[name] = data
+end)
+
+RegisterNetEvent("omik-callback:TriggerClientCallback")
+AddEventHandler("omik-callback:TriggerClientCallback", function(name, args)
+    TriggerServerEvent("omik-callback:RecieveClientCallback", name, cCallback.client[name](table.unpack(args)))
 end)
